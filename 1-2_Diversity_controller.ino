a9001f1dbf4be5e966d38b1e4000e25bec3239c5
Rusty105 <rustyppc@gmail.com>
2019-02-23 00:57:42 -0500

    Just more progress

--- 1-2_Diversity_controller.ino
+++ 1-2_Diversity_controller.ino
@@ -7,6 +7,10 @@
 // Target board = Arduino Mini Pro (5V)
 
 
+#include "U8glib.h"
+
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE);  // I2C / TWI 
+
 // Setup variables
 
 // ADC input pins
@@ -16,9 +20,9 @@ int rxb = 1;  // ADC input of reciever B
 // int rxd = ?;  // ADC input of reciever D (future)
 
 // Manual button  pins
-int manauto = 1; // Button to automatically pick reciever
-int mana = 1;  // Button to manualy pick reciever A
-int manb = 1;  // Button to manualy pick reciever B
+int manauto = 3; // Button to automatically pick reciever
+int mana = 4;  // Button to manualy pick reciever A
+int manb = 2;  // Button to manualy pick reciever B
 // int manc = ?;  // Button to manualy pick reciever C (future)
 // int mand = ?;  // Button to manualy pick reciever D (future)
 int mode = 0; // diversity mode on startup, 0=Auto, 1= rx A, 2= rx B, etc...
@@ -44,10 +48,10 @@ void setup() {
    Serial.begin(9600); // setup serial for debug
    pinMode(rxa, INPUT); // rx A input
    pinMode(rxb, INPUT); // rx B input
-
-   pinMode(manauto, INPUT); // input for Auto mode
-   pinMode(mana, INPUT);  // input for manually select rx A
-   pinMode(manb, INPUT);  // input for manually select rx B
+// use internal pullups, buttons will pull to low
+   pinMode(manauto, INPUT_PULLUP); // input for Auto mode
+   pinMode(mana, INPUT_PULLUP);  // input for manually select rx A
+   pinMode(manb, INPUT_PULLUP);  // input for manually select rx B
 
    pinMode(leda, OUTPUT); // rx A LED (likely use built in LED for rx a)
    pinMode(ledb, OUTPUT); // rx B LED
@@ -65,10 +69,10 @@ void loop(){
    int buttonauto = digitalRead(manauto);
   
 // set RX mode
-   if (buttona = 1){
+   if (buttona = 0){
       mode=1;  // set rx A
    }
-   else if (buttonb = 1){
+   else if (buttonb = 0){
       mode=2;  // set rx B
    }else
       mode=0;  // set auto
@@ -84,6 +88,10 @@ void loop(){
    float voltb = RSSIB*(5.0/1023.0);
 
 // display readings
+ u8g.firstPage();  
+  do {
+    draw();
+  } while( u8g.nextPage() );
 
 // decide on reciever
 
@@ -105,6 +113,7 @@ void setrxa(){
    userx=1;
    digitalWrite(ledb,LOW); // set ledb off
    digitalWrite(leda,HIGH); // set leda on
+   // send output to switch
    tone(buzz, tonefreqa);
    delay(100); // 1000 = 1 sec
    noTone(buzz);
@@ -116,7 +125,19 @@ void setrxb(){
    userx=2;
    digitalWrite(leda,LOW); // set leda off
    digitalWrite(ledb,HIGH); // set ledb on
+   // sens output to switch
    tone(buzz, tonefreqb);
    delay(100); // 1000 = 1 sec
    noTone(buzz);
+}
+
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+  u8g.setFont(u8g_font_unifont);
+  //u8g.setFont(u8g_font_osb21);
+  u8g.drawStr( 0, 22, "RX-A  4.23V  97%");
+  u8g.drawStr( 0, 44, "RX-B  3.23V  84%");
+  u8g.drawStr( 0, 60, "Counter " " units");
+
+
 }
